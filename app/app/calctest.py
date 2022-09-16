18fcc729f227163d4500d5c84e415abb5aa2ec22
kliff <c.thelusca@hotmail.com>
2022-09-16 14:48:18 -0400

    updates

--- app/app/calctest.py
+++ app/app/calctest.py
@@ -3,7 +3,10 @@ Calculator functions
 
 """
 
+
 def add(x, y):
     return x + y
+
+
 def sub(x, y):
     return x - y
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -2,42 +2,34 @@
 Test Custom Django management Commands
 """
 
-from unittest.mock import patch # mock the behavoir (stimilation )
+from unittest.mock import patch  # mock the behavoir (stimilation )
 
-from psycopg2 import OperationalError as Psycopg2OpError  # might be a connection error?
+#  might be a connection error?
+from psycopg2 import OperationalError as Psycopg2OpError
 
-from django.core.management import call_command # allow to call command
-from django.db.utils import OperationalError # Error #2?
-from django.test import SimpleTestCase # For Testing
-from app import calctest
+from django.core.management import call_command  # allow to call command
+from django.db.utils import OperationalError  # Error #2?
+from django.test import SimpleTestCase  # For Testing
 
-class Calculator_Test(SimpleTestCase):
+# .check = mock object/ value
 
-    def test_add(self):
-        res = calctest.add(5, 4)
 
-        self.assertEqual(res, 9 )
-    def test_sub(self):
-        res = calctest.sub(5, 4)
-
-        self.assertEqual(res, 1)
-
-@patch('core.management.commands.wait_for_db.Command.check') # .check = mock object/ value
+@patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
-
-    def test_wait_for_db_ready(self, patched_check): # patched checked because of @patch
+    # patched checked because of @patch
+    def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
-        patched_check.return_value = True #when called return True Value
+        patched_check.return_value = True  # when called return True Value
 
         call_command('wait_for_db')
-
-        patched_check.assert_called_once_with(databases=['default']) #check if called with databases=['default']
+        #  check if called with databases=['default']
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
-        
+        # first 2 times call mock method, the next 3 times raise Oerror
         patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
 
